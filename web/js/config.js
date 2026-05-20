e18a03371418e0c1d837451bf7ff9d006e076741
Andrey <andrey@MacBook-Pro-Andrey.local>
2026-05-20 12:06:40 +0500

    Sync release version to 1.2.9

--- web/js/config.js
+++ web/js/config.js
@@ -1,4 +1,4 @@
-export const APP_RELEASE = '1.2.8';
+export const APP_RELEASE = '1.2.9';
 export const PROFILE_NO_CHANGES_LABEL = 'Профиль актуален';
 export const PROFILE_NO_CHANGES_MESSAGE = 'Профиль актуален';
 
